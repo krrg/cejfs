0b5832f97d351eee8fab43da790b1fd446dd92aa
Ken Reese <krrgithub@gmail.com>
2017-10-16 12:51:59 -0600

    Gradled

---
+++ src/main/java/isrl/byu/edu/Main.java
@@ -0,0 +1,15 @@
+package isrl.byu.edu;
+
+import isrl.byu.edu.examples.HelloFuse;
+
+/**
+ * Created by krr428 on 10/16/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+
+
+    }
+
+}
