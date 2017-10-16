6ecb03535adaede81740fb27415043628d2b9a1c
Ken Reese <krrgithub@gmail.com>
2017-10-16 11:41:34 -0600

    Gradled

---
+++ src/edu/byu/isrl/Main.java
@@ -0,0 +1,12 @@
+package edu.byu.isrl;
+
+/**
+ * Created by krr428 on 10/16/17.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello world");
+    }
+
+}
